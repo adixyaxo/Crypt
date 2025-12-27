ded9100cfdda6c146e3c5d3f201e8f88a80fe833
adixyxo <aditya0dagar@gmail.com>
2025-12-27 15:41:12 +0530

    starting to develop the core logic for the project

---
+++ logic.c
@@ -0,0 +1,10 @@
+// This file would contain core cryptography logic
+#include <stdio.h>
+
+int clogic()
+{
+    char key[50];
+    scanf(" %s", key);
+
+    return 0;
+}
---
+++ ui.c
@@ -0,0 +1,1 @@
+// This file would contain the ui of the game created using gtk
